794aa99d0badbb744c4795cf0c229b11be3e0f32
Bharat Dhakad <bdhakad166@gmail.com>
2026-05-08 13:09:46 +0530

    fix vercel last

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -4,7 +4,7 @@ import { updateProfile } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey:  import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey:  import.meta.env.VITE_FIREBASE_API_ID,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
