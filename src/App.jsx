1a130cadf6fd3c3b9ae51922c9bdb0c283f73874
Bharat Dhakad <bdhakad166@gmail.com>
2026-05-08 11:24:12 +0530

    Fix build issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./components/NavBar"
-import LoginPage from "./pages/LoginPage"
-import MenuPage from "./pages/MenuPage"
-import CartPage from "./pages/CartPage"
-import OrdersPage from "./pages/OrdersPage"
-import HomePage from "./pages/HomePage"
-import ProfilePage from './pages/ProfilePage'
-import Footer from "./components/Footer"
-import SignupPage from "./pages/Signup"
+import Navbar from "./components/NavBar";
+import LoginPage from "./pages/LoginPage";
+import MenuPage from "./pages/MenuPage";
+import CartPage from "./pages/CartPage";
+import OrdersPage from "./pages/OrdersPage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from './pages/ProfilePage';
+import Footer from "./components/Footer";
+import SignupPage from "./pages/Signup";
 import ProductPage from "./pages/ProductPage";
 
 function App() {
